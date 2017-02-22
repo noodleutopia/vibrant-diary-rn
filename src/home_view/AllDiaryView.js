2d0416ee715279f6c765ee8b832dc6081f39719f
zhangyf <zhangyafei@outlook.com>
2017-02-22 18:34:24 +0800

    add diary list.
    Signed-off-by: zhangyf <zhangyafei@outlook.com>

--- src/home_view/AllDiaryView.js
+++ src/home_view/AllDiaryView.js
@@ -10,6 +10,8 @@ import{
 import Button from '../components/Button';
 import Reflux from 'reflux';
 import DiaryStore from '../stores/DiaryStore';
+import {dateTimeHelper} from '../utils/DateFormatUtil'
+
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 class AllDiaryView extends Reflux.Component {
@@ -25,13 +27,38 @@ class AllDiaryView extends Reflux.Component {
 
     return(
       <View style={styles.container}>
+        <View style={styles.top}>
+          <Button style={{ position: 'absolute', width: 60, left: 0, margin: 0, padding: 0}} text={"返回"} onPress={this.props.quit}/>
+          <Text style={{textAlign:'center', fontSize: 15,}}>日记列表</Text>
+        </View>
         <ListView
           dataSource={ds.cloneWithRows(this.state.diarys)}
           renderRow={(rowData) => <DiaryListItem rowData={rowData}/>}
+          renderSeparator={this._renderSeperator}
+          renderSectionHeader={this.renderSectionHeader}
         />
       </View>
     )
   }
+
+  _renderSeperator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+    return (
+      <View
+        key={`${sectionID}-${rowID}`}
+        style={{
+          height: adjacentRowHighlighted ? 4 : 1,
+          backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+        }}
+      />
+    );
+  }
+
+  renderSectionHeader(sectionData, sectionID){
+    console.log('sectionData: ' , sectionData);
+      return(
+          <Text style={{backgroundColor:'yellow'}}>Section</Text>
+      );
+    }
 }
 
 AllDiaryView.propTypes = {
@@ -46,16 +73,17 @@ class DiaryListItem extends Component {
 
   _onPress (item) {
     console.log('你点击了Item：', item);
-    this.props.preview(this.props.itemId);
+    // this.props.preview(this.props.itemId);
   }
 
   render() {
     return(
       <TouchableOpacity
-        onPress={()=>this._onPress(this.props.item)}
+        onPress={()=>this._onPress(this.props.rowData)}
         style={styles.listItem}>
         <View>
-          <Text>{_date.getFullYear()}年{_date.getMonth()+1}月{_date.getDate()}日{'\n'+this.getXingqi(_date.getDay())}</Text>
+          <Text>{dateTimeHelper.getInstance().format(this.props.rowData.date) + " " 
+            + dateTimeHelper.getInstance().xingqi(this.props.rowData.date)}</Text>
         </View>
       </TouchableOpacity>
     );
@@ -65,14 +93,26 @@ class DiaryListItem extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00e700',
+    paddingTop:20,
+    // backgroundColor: '#00e700',
     // justifyContent: 'center',
     // alignItems: 'center'
+  },
+  top: {
+    height: 30,
+    backgroundColor: '#00e700',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   title: {
     // flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  listItem: {
+    height: 40,
+    padding: 10,
   },
 });
 
